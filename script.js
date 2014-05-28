f0af0eb55d3d95d103d83417116df20a6c35ddb6
Lacrymoza <zare010@gmail.com>
2014-05-28 22:38:22 +0600

    fun

--- script.js
+++ script.js
@@ -151,9 +151,8 @@ function init() {
 }
 
 
-function showGroup(group)
-{
-    myMap.setCenter([41.20, 74.00], 6, { duration: 20000 });
+function showGroup(group) {
+    myMap.setCenter([41.20, 74.00], 6, { duration: 1000 });
     for (var i = 0; i < group.length; i++) {
         group[i].options.set({ visible: true });
     }
